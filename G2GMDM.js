f89fd6096a21506ea1b5378aa54f02daf5b47d0e
Abdiel Isai <ozorio.abdiel@gmail.com>
2017-04-27 21:07:22 -0500

    Update G2GMDM.js
    Zoom sync added

--- G2GMDM.js
+++ G2GMDM.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME G2GMDM
 // @name:es      WME Vamoj pa Gaia
-// @version      0.2
+// @version      0.3
 // @description  WME Go to GaiaMDM. Redirects to Gaia MDM page on current position by clicking on wme coordinates label.
 // @description:es WME Vamoj pa Gaia te lleva directito a la mijma posicion en el mapa de gaia pa que no tengas que hacer tantos clics puej.
 // @author       abdielisai
@@ -35,22 +35,21 @@
     bootstrap();
 
     function init(){
-        //> Finds the element containing the coordinates on Waze Map Editor page
-        var elements = document.getElementsByClassName("mouse-position");
-        if(elements.length > 0){
-            //> Listens on click event 
-            elements[0].onclick = function(){
-                //> Extracts coordinates from element inner text and encode them into base64
-                var coords = elements[0].innerText.split(" ");
-                window.open("http://gaia.inegi.org.mx/mdm6/?v="+btoa("lat:"+coords[1]+",lon:"+coords[0]+",z:13"));
-            };
-        }
+        //> Listens on click event 
+        $(".mouse-position")[0].onclick = go2GMDM;
+
         console.log("WME G2GMDM " + GM_info.script.version+" is running.");
+    }
+
+    function go2GMDM(){
+        //> Extracts coordinates from element inner text and encode them into base64
+        var coords = $(".mouse-position")[0].innerText.split(" ");
+        window.open("http://gaia.inegi.org.mx/mdm6/?v="+btoa("lat:"+coords[1]+",lon:"+coords[0]+",z:"+(W.map.zoom+8)));
     }
 })();
 
 //> You can paste this section into the console to try it without install it
-// document.getElementsByClassName("mouse-position")[0].onclick = function(){
-//     var coords = document.getElementsByClassName("mouse-position")[0].innerText.split(" ");
-//     window.open("http://gaia.inegi.org.mx/mdm6/?v="+btoa("lat:"+coords[1]+",lon:"+coords[0]+",z:13"));
+// $(".mouse-position")[0].onclick = function(){
+//     var coords = $(".mouse-position")[0].innerText.split(" ");
+//     window.open("http://gaia.inegi.org.mx/mdm6/?v="+btoa("lat:"+coords[1]+",lon:"+coords[0]+",z:"+(W.map.zoom+8)));
 // };
